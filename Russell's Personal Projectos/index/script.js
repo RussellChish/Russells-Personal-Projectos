f1b0b2e8b371faad8934d1c3d91971b4919748e6
Russell Chishaka <166718127+RussellChish@users.noreply.github.com>
2024-05-21 07:16:39 +0200

    Add files via upload

---
+++ Russell's Personal Projectos/index/script.js
@@ -0,0 +1,11 @@
+// const images = ['../images/pexels-madvortex-8957613.jpg','../images/pexels-lumn-322207.jpg' ];
+// let imageIndex = 0;
+
+// function switchImage(){
+//     console.log(imageIndex);
+//     const imageElement = document.getElementById('heroimg');
+//     imageIndex = (imageIndex + 1) % images.length;
+//     imageElement.src = images[imageIndex];
+// }
+
+// setInterval(switchImage, 5000);
